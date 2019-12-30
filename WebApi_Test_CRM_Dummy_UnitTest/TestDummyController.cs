bbae7a5fd0b9f17ac79838057dcd01b21be9b3fe
20337606688 <20337606688@WS61661>
2019-12-30 16:06:09 -0300

    TestDummyController

--- WebApi_Test_CRM_Dummy_UnitTest/TestDummyController.cs
+++ WebApi_Test_CRM_Dummy_UnitTest/TestDummyController.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApi_Test_CRM_Dummy.Controllers;
 
 namespace WebApi_Test_CRM_Dummy_UnitTest
 {
@@ -15,11 +16,20 @@ namespace WebApi_Test_CRM_Dummy_UnitTest
         
 
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldGetEmployees()
         {
+
+            //AAA
+
             //
-            // TODO: Agregar aquí la lógica de las pruebas
-            
+            DummyController controller = new DummyController();
+
+            //Action
+            object result = controller.Get();
+            //Assert
+            Assert.IsNotNull(result);
+
+
         }
     }
 }
